2bc5d5f95a2bdb3071627db3ac5bc7924f6280ba
acl13 <128934431+acl13@users.noreply.github.com>
2024-06-18 15:33:01 -0400

    add event listener to dropdown btn; use imported func from dropdown-display.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Is this thing on?');
+import './style.css';
+import { dropdownDisplay } from './dropdown-display';
+
+const dropdownBtns = document.querySelectorAll('.dropdown-btn');
+dropdownBtns.forEach((item) => {
+    console.log('test');
+    item.addEventListener('click', dropdownDisplay);
+}) 
